959b45ae5c74e16e06d4cdd5cb2d99a81c8854c5
Thomas Fossati <thomas.fossati@arm.com>
2023-01-26 15:34:59 +0000

    add barebones CLI that signs its stdin using COSE Sign1
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/thomas-fossati/go-cose-cli
+
+go 1.19
+
+require (
+	github.com/lestrrat-go/jwx/v2 v2.0.8
+	github.com/veraison/go-cose v1.0.0
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/veraison/go-cose"
+)
+
+func nameToAlg(name string) (error, cose.Algorithm) {
+	switch name {
+	case "ES256":
+		return nil, cose.AlgorithmES256
+	case "ES384":
+		return nil, cose.AlgorithmES384
+	default:
+		return fmt.Errorf("algorithm %s not supported", name), 0
+	}
+}
+
+func MustLoadSignerFromFile(sAlg, sKeyFile string) cose.Signer {
+	rawSKey, err := ioutil.ReadFile(sKeyFile)
+	if err != nil {
+		log.Fatalf("unable to read private key file: %v", err)
+	}
+
+	var skey crypto.PrivateKey
+
+	err = jwk.ParseRawKey(rawSKey, &skey)
+	if err != nil {
+		log.Fatalf("parsing JWK file: %v", err)
+	}
+
+	ecKey, ok := skey.(*ecdsa.PrivateKey)
+	if !ok {
+		log.Fatalf("want EC key, got %T", skey)
+	}
+
+	err, alg := nameToAlg(sAlg)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	signer, err := cose.NewSigner(alg, ecKey)
+	if err != nil {
+		log.Fatalf("creating signer: %v", err)
+	}
+
+	return signer
+}
+
+func main() {
+	var sAlg, sKeyFile string
+
+	flag.StringVar(&sAlg, "a", "", "signature algorithm (ES256, ES384)")
+	flag.StringVar(&sKeyFile, "k", "", "file containing a private key in JWK format")
+
+	flag.Parse()
+
+	signer := MustLoadSignerFromFile(sAlg, sKeyFile)
+
+	headers := cose.Headers{}
+
+	r := bufio.NewReader(os.Stdin)
+
+	payload, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatalf("reading from stdin: %s", err)
+	}
+
+	sig, err := cose.Sign1(rand.Reader, signer, headers, payload, nil)
+	if err != nil {
+		log.Fatalf("sign1 failed: %v", err)
+	}
+
+	fmt.Println("message signed")
+
+	fmt.Printf("%x\n", sig)
+}
